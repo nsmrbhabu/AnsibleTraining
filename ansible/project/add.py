a755625eb2ebf7a08745aaf510cf20f0baaa987d
nsmrbhabu <44363797+nsmrbhabu@users.noreply.github.com>
2018-10-26 05:24:31 -0700

    Add files via upload

---
+++ ansible/project/add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#The below is function definition
+def add():
+    add1 = input('enter a number : ')
+    add2 = input('enter a number : ')
+    return add1 + add2
+
+#Invoke funtion
+print ('The result is ' + str( add()))
---
+++ ansible/project/printfuntion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#The below is function definition
+def sayhello():
+    print 'hello - statement'
+    print ('hello - function')
+
+#Invoke funtion
+sayhello()
